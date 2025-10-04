5cd645e6feddfa9fadc75230a3e33e7162e16976
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-05 02:04:13 +0300

    add routes at app file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Categoris } from './components/manage/categoris/categoris';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Home, title:'Ecommerce' },
+    {path:'admin/categories',component:Categoris, title:'Ecommerce Categories' },
+    {path:'admin/categories/add',component:Categoris, title:'Ecommerce Categories' },
+    {path:'admin/categories/:id',component:Categoris, title:'Ecommerce Categories' },
+];
