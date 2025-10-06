b387c3a4f0d17fdc7158be27bd48ebe42a56d438
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-06 19:35:34 +0300

    fetch apis at category service

--- src/app/Services/category.ts
+++ src/app/Services/category.ts
@@ -1,13 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { CategoryInterface } from '../interfaces/category-interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Category {
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  getCategory(){
-    return this.http.get('')
+  getCategory() {
+    return this.http.get<CategoryInterface[]>(`${environment.api_base_url}/category`);
+  }
+  getCategoryById(id: string) {
+    return this.http.get<CategoryInterface>(`${environment.api_base_url}/category/${id}`);
+  }
+  addCategory(name: string) {
+    return this.http.post(`${environment.api_base_url}/category`, {
+      name: name,
+    });
+  }
+  updateCategory(id: string, name: string) {
+    return this.http.put(`${environment.api_base_url}/category/${id}`, {
+      name: name,
+    });
+  }
+  delteCategory(id:string){
+    return this.http.delete(`${environment.api_base_url}/category/${id}`)
   }
 }
