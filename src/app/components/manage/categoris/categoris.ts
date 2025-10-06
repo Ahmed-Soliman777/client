f3664d3633f0537f75f3aa7139a746c9634ca234
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-06 19:34:49 +0300

    add logic to category component

--- src/app/components/manage/categoris/categoris.ts
+++ src/app/components/manage/categoris/categoris.ts
@@ -1,25 +1,50 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { Category } from '../../../Services/category';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { CategoryInterface } from '../../../interfaces/category-interface';
 
 @Component({
   selector: 'app-categoris',
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './categoris.html',
   styleUrl: './categoris.css',
 })
-export class Categoris {
-  displayedColumns: string[] = ['id', 'name', 'action'];
-  dataSource: MatTableDataSource<any>;
+export class Categoris implements OnInit {
+  displayedColumns: string[] = ['_id', 'name', 'action'];
+  dataSource: MatTableDataSource<CategoryInterface>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
+  constructor(private categoryService: Category) {
+    this.categoryService.getCategory().subscribe((result: any) => {
+      console.log(result);
+    });
     this.dataSource = new MatTableDataSource([] as any);
+  }
+
+  ngOnInit() {
+    this.getServerData();
+  }
+
+  private getServerData() {
+    this.categoryService.getCategory().subscribe((result) => {
+      this.dataSource.data = result;
+    });
   }
 
   ngAfterViewInit() {
@@ -34,5 +59,12 @@ export class Categoris {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  deleteItem(id: string) {
+    this.categoryService.delteCategory(id).subscribe((result: any) => {
+      alert('Category deleted!');
+      this.getServerData();
+    });
   }
 }
