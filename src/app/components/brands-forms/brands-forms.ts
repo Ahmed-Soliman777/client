6176657a17d8aab14657311c62410fb2d4729b19
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-07 00:09:06 +0300

    add form angular ui functions

--- src/app/components/brands-forms/brands-forms.ts
+++ src/app/components/brands-forms/brands-forms.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BrandServices } from './../../Services/brand-services';
 
 @Component({
   selector: 'app-brands-forms',
-  imports: [],
+  imports: [FormsModule, MatButtonModule, MatInputModule],
   templateUrl: './brands-forms.html',
-  styleUrl: './brands-forms.css'
+  styleUrl: './brands-forms.css',
 })
-export class BrandsForms {
-
+export class BrandsForms implements OnInit {
+  name!: string;
+  id!: string;
+  isEdit: boolean = false;
+  constructor(
+    private brandServices: BrandServices,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    console.log(this.id);
+    if (this.id) {
+      this.isEdit = true;
+      this.brandServices.getBrandsById(this.id).subscribe((result: any) => {
+        console.log(result);
+        this.name = result.name;
+      });
+    }
+  }
+  add() {
+    console.log(this.name);
+    this.brandServices.addBrand(this.name).subscribe((result: any) => {
+      alert('brand added!');
+      this.router.navigateByUrl('/admin/brands');
+    });
+  }
+  update() {
+    console.log(this.name);
+    this.brandServices.updateBrand(this.id, this.name).subscribe((result: any) => {
+      alert('brand updated!');
+      this.router.navigateByUrl('/admin/brands');
+    });
+  }
 }
--- src/app/components/brands/brands.ts
+++ src/app/components/brands/brands.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrandServices } from './../../Services/brand-services';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { BrandInterface } from './../../interfaces/brand-interface';
 
 @Component({
   selector: 'app-brands',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './brands.html',
-  styleUrl: './brands.css'
+  styleUrl: './brands.css',
 })
-export class Brands {
+export class Brands implements OnInit {
+  displayedColumns: string[] = ['_id', 'name', 'action'];
+  dataSource: MatTableDataSource<BrandInterface>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private brandService: BrandServices) {
+    this.brandService.getBrands().subscribe((result: any) => {
+      console.log(result);
+    });
+    this.dataSource = new MatTableDataSource([] as any);
+  }
+
+  ngOnInit() {
+    this.getServerData();
+  }
+
+  private getServerData() {
+    this.brandService.getBrands().subscribe((result) => {
+      this.dataSource.data = result;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteItem(id: string) {
+    this.brandService.deleteBrand(id).subscribe((result: any) => {
+      alert('Brand deleted!');
+      this.getServerData();
+    });
+  }
 }
