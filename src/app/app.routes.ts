f795d0d2d7c12a7a41e08a465598a09f1ac03c28
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-06 23:40:53 +0300

    add brands routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Categoris } from './components/manage/categoris/categoris';
 import { CategoryForm } from './components/category-form/category-form';
+import { Brands } from './components/brands/brands';
+import { BrandsForms } from './components/brands-forms/brands-forms';
 
 export const routes: Routes = [
-    {path:'',component:Home, title:'Ecommerce' },
-    {path:'admin/categories',component:Categoris, title:'Ecommerce Categories' },
-    {path:'admin/categories/add',component:CategoryForm, title:'Ecommerce Categories' },
-    {path:'admin/categories/:id',component:CategoryForm, title:'Ecommerce Categories' },
+  { path: '', component: Home, title: 'Ecommerce' },
+  { path: 'admin/categories', component: Categoris, title: 'Ecommerce Categories' },
+  { path: 'admin/categories/add', component: CategoryForm, title: 'Ecommerce Categories' },
+  { path: 'admin/categories/:id', component: CategoryForm, title: 'Ecommerce Categories' },
+  { path: 'admin/categories/:id', component: CategoryForm, title: 'Ecommerce Categories' },
+  { path: 'admin/brands', component: Brands, title: 'Ecommerce Bands' },
+  { path: 'admin/brands/add', component: BrandsForms, title: 'Ecommerce Bands' },
+  { path: 'admin/brands/:id', component: BrandsForms, title: 'Ecommerce Bands' },
+  { path: 'admin/brands/:id', component: BrandsForms, title: 'Ecommerce Bands' },
 ];
