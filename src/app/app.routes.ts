a522325469b4b21fb48d12ec7c114000975312ae
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-06 19:32:35 +0300

    add category id page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Categoris } from './components/manage/categoris/categoris';
+import { CategoryForm } from './components/category-form/category-form';
 
 export const routes: Routes = [
     {path:'',component:Home, title:'Ecommerce' },
     {path:'admin/categories',component:Categoris, title:'Ecommerce Categories' },
-    {path:'admin/categories/add',component:Categoris, title:'Ecommerce Categories' },
-    {path:'admin/categories/:id',component:Categoris, title:'Ecommerce Categories' },
+    {path:'admin/categories/add',component:CategoryForm, title:'Ecommerce Categories' },
+    {path:'admin/categories/:id',component:CategoryForm, title:'Ecommerce Categories' },
 ];
