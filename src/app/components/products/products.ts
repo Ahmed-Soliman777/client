c4a21c05874106de578ef9f41af9f011f8c9a398
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-08 03:08:49 +0300

    add logic

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ProductServices } from './../../Services/product-services';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { ProductInterface } from './../../interfaces/product-interface';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './products.html',
-  styleUrl: './products.css'
+  styleUrl: './products.css',
 })
-export class Products {
+export class Products implements OnInit {
+  displayedColumns: string[] = ['_id', 'name', 'shortDescription', 'discount', 'action'];
+  dataSource: MatTableDataSource<ProductInterface>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private productService: ProductServices) {
+    this.productService.getProducts().subscribe((result: any) => {
+      console.log(result);
+    });
+    this.dataSource = new MatTableDataSource([] as any);
+  }
+
+  ngOnInit() {
+    this.getServerData();
+  }
+
+  private getServerData() {
+    this.productService.getProducts().subscribe((result) => {
+      this.dataSource.data = result;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteItem(id: string) {
+    this.productService.deleteProduct(id).subscribe((result: any) => {
+      alert('Product deleted!');
+      this.getServerData();
+    });
+  }
 }
