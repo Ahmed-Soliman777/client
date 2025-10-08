c4a81fca0a1ca1e7999b2a42cf0c05efeb3888e1
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-08 22:16:02 +0300

    add routerlink module

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Category } from '../../Services/category';
+import { CategoryInterface } from '../../interfaces/category-interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
-export class Header {
-
+export class Header implements OnInit {
+  categoryCollection: CategoryInterface[] = [];
+  constructor(private CategoryServices: Category) {}
+  ngOnInit(): void {
+    this.CategoryServices.getCategory().subscribe((result) => {
+      this.categoryCollection = result;
+    });
+  }
 }
