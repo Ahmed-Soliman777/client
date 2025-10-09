156c6722b3064ca8b6206f8ab57ba81a9e219baf
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-09 14:41:46 +0300

    add carousel to home component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -3,23 +3,51 @@ import { ProductServices } from '../../Services/product-services';
 import { ProductInterface } from '../../interfaces/product-interface';
 import { CurrencyPipe } from '@angular/common';
 import { ProductCard } from '../product-card/product-card';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
-  imports: [CurrencyPipe, ProductCard],
+  imports: [CurrencyPipe, ProductCard, CarouselModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
   newProducts: ProductInterface[] = [];
   featuredProducts: ProductInterface[] = [];
+  bannerImages: ProductInterface[] = [];
   constructor(private productServices: ProductServices) {}
-  ngOnInit(): void {
-    this.productServices.getNewProducts().subscribe((result) => {
-      this.newProducts = result;
-    });
+    ngOnInit(): void {
+      this.productServices.getNewProducts().subscribe((result) => {
+        this.newProducts = result;
+        this.bannerImages.push(...result)
+      });
     this.productServices.getFeaturedProducts().subscribe((result) => {
       this.featuredProducts = result;
+        this.bannerImages.push(...result)
     });
   }
 }
