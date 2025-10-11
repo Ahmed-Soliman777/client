0a6df190411c8735972fc91c2e00ca7948d280e0
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-11 14:44:23 +0300

    add reactive form module for register

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatFormField, MatInputModule, MatLabel } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.css'
+  imports: [MatInputModule, MatFormField, MatLabel, FormsModule, ReactiveFormsModule, NgClass],
+templateUrl: './register.html',
+  styleUrl: './register.css',
 })
-export class Register {
-
+export class Register implements OnInit {
+  registerForm!: FormGroup;
+  constructor(private formbuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.registerForm = this.formbuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+  register() {
+    console.log(this.registerForm.value);
+  }
 }
