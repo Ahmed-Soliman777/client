6a15bb411557a2225d5d704f67331486b26eefcb
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-11 15:06:47 +0300

    fetch register api

--- src/app/Services/auth.ts
+++ src/app/Services/auth.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Auth {
-  
+  constructor(private http: HttpClient) {}
+  register(username: string, email: string, password: string) {
+    return this.http.post(`${environment.api_base_url}/register`, {
+      username,
+      email,
+      password,
+    });
+  }
 }
