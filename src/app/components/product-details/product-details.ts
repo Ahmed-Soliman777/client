cb6177fae563c8ccfa09fad15c2f7318234eec84
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-13 01:48:23 +0300

    fix issues

--- src/app/components/product-details/product-details.ts
+++ src/app/components/product-details/product-details.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductServices } from '../../Services/product-services';
 import { ProductInterface } from '../../interfaces/product-interface';
 import { CurrencyPipe } from '@angular/common';
@@ -7,7 +7,7 @@ import { ProductCard } from '../product-card/product-card';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CurrencyPipe, ProductCard],
+  imports: [CurrencyPipe, ProductCard, RouterLink],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
@@ -18,6 +18,24 @@ export class ProductDetails implements OnInit {
   constructor(private route: ActivatedRoute, private productServices: ProductServices) {}
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
+
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.productServices.getProductsById(id).subscribe({
+          next: (result) => {
+            this.product = result;
+
+            if (this.product.images && this.product.images.length > 0) {
+              this.mainImage = this.product.images[0];
+            }
+            this.loadSimilarProducts(this.product.categoryId);
+          },
+          error: (err) => console.error('Error loading product:', err),
+        });
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    });
 
     this.productServices.getProductsById(id).subscribe({
       next: (result) => {
