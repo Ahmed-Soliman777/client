7d849576c4c0576caab456ea93ab9ba0b776c0a9
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-13 01:32:21 +0300

    add similar products

--- src/app/components/product-details/product-details.ts
+++ src/app/components/product-details/product-details.ts
@@ -3,24 +3,42 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductServices } from '../../Services/product-services';
 import { ProductInterface } from '../../interfaces/product-interface';
 import { CurrencyPipe } from '@angular/common';
+import { ProductCard } from '../product-card/product-card';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, ProductCard],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
 export class ProductDetails implements OnInit {
   product!: ProductInterface;
   mainImage!: string;
+  similarProducts: ProductInterface[] = [];
   constructor(private route: ActivatedRoute, private productServices: ProductServices) {}
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
-    this.productServices.getProductsById(id).subscribe((result) => {
-      this.product = result;
-      this.mainImage = this.product.images[0];
+
+    this.productServices.getProductsById(id).subscribe({
+      next: (result) => {
+        this.product = result;
+
+        if (this.product.images && this.product.images.length > 0) {
+          this.mainImage = this.product.images[0];
+        }
+        this.loadSimilarProducts(this.product.categoryId);
+      },
+      error: (err) => console.error('Error loading product:', err),
     });
   }
+
+  loadSimilarProducts(categoryId: string) {
+    this.productServices.getProductList('', categoryId, '', 1, 4, '', -1).subscribe({
+      next: (result) => (this.similarProducts = result),
+      error: (err) => console.error('Error loading similar products:', err),
+    });
+  }
+
   changeImage(item: string) {
     this.mainImage = item;
   }
