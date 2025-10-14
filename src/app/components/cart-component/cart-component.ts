fd52227231ff3b79932d41f03a25711ab7f25a09
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 01:08:05 +0300

    add cart component logic

--- src/app/components/cart-component/cart-component.ts
+++ src/app/components/cart-component/cart-component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from '../../Services/cart';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-component',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart-component.html',
-  styleUrl: './cart-component.css'
+  styleUrl: './cart-component.css',
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  constructor(public cartService: Cart) {}
+  ngOnInit(): void {
+    this.cartService.init();
+  }
+  get cartItems() {
+    return this.cartService.items;
+  }
+  removeCart(id:string){
+    this.cartService.removeFromCart(id).subscribe(()=>{
+          this.cartService.init();
+    })
+  }
+  increaseQuantity(id:string, quantity:number){
+    this.cartService.addToCart(id,quantity).subscribe(()=>{
+      this.cartService.init();
+    })
+  }
+  decreaseQuantity(id:string, quantity:number){
+    this.cartService.addToCart(id,quantity).subscribe(()=>{
+      this.cartService.init();
+    })
+  }
+  get totalPrice(): number {
+  return this.cartService.items.reduce(
+    (sum, item) => sum + item.quantity * item.product.sellingPrice,
+    0
+  );
+}
 
 }
