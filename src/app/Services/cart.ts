179b5ca964212dcf52400a780c381b250d5836f5
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-14 22:40:50 +0300

    add cart services

--- src/app/Services/cart.ts
+++ src/app/Services/cart.ts
@@ -1,8 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ProductInterface } from '../interfaces/product-interface';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Cart {
-  
+  items: { product: ProductInterface; quantity: number }[] = [];
+  constructor(private http: HttpClient) {}
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      token: token || '',
+    });
+  }
+  init() {
+    this.getCartItems().subscribe((result) => {});
+  }
+  getCartItems() {
+    return this.http.get<{ product: ProductInterface; quantity: number }[]>(
+      `${environment.api_base_url}/cart`,
+      { headers: this.getHeaders() }
+    );
+  }
+  addToCart(productId: string, quantity: number) {
+    return this.http.post(
+      `${environment.api_base_url}/cart/${productId}`,
+      {
+        quantity: quantity,
+      },
+      { headers: this.getHeaders() }
+    );
+  }
+  removeFromCart(productId: string) {
+    return this.http.delete(`${environment.api_base_url}/cart/${productId}`, {
+      headers: this.getHeaders(),
+    });
+  }
 }
