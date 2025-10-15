db4811a057c70a5a481264d0c07a112cb342e9b3
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 16:44:37 +0300

    fetch get orders

--- src/app/Services/order-services.ts
+++ src/app/Services/order-services.ts
@@ -1,8 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { OrderInterface } from '../interfaces/order-interface';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderServices {
-  
+  constructor(public http: HttpClient) {}
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      token: token || '',
+    });
+  }
+  addOrder(order: OrderInterface) {
+    return this.http.post(`${environment.api_base_url}/order`, order, {
+      headers: this.getHeaders(),
+    });
+  }
+  getOrders() {
+    return this.http.get<OrderInterface[]>(`${environment.api_base_url}/order`, {
+      headers: this.getHeaders(),
+    });
+  }
 }
