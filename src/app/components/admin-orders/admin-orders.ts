1ba59541dd89624bd99b052cfacf796bb67dc4b2
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 21:29:35 +0300

    add admin order services

--- src/app/components/admin-orders/admin-orders.ts
+++ src/app/components/admin-orders/admin-orders.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderServices } from '../../Services/order-services';
+import { OrderInterface } from '../../interfaces/order-interface';
+import { CurrencyPipe } from '@angular/common';
+import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-admin-orders',
-  imports: [],
+  imports: [CurrencyPipe, MatButtonToggleModule],
   templateUrl: './admin-orders.html',
-  styleUrl: './admin-orders.css'
+  styleUrl: './admin-orders.css',
 })
-export class AdminOrders {
+export class AdminOrders implements OnInit {
+  orders: OrderInterface[] = [];
+  constructor(private orderService: OrderServices) {}
+  ngOnInit() {
+    this.orderService.getAllOrders().subscribe((result) => {
+      this.orders = result;
+    });
+  }
 
+  statusChanged(event: MatButtonToggleChange, order: OrderInterface) {
+  const newStatus = event.value;
+  this.orderService.updateOrder(order._id!, newStatus).subscribe(() => {
+    alert(`Order status updated to ${newStatus}`);
+  });
+}
 }
