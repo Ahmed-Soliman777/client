6c0237ff2bdd7ff4bfa57a78fef832af99e4c477
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 16:42:50 +0300

    add order id to order interface

--- src/app/interfaces/order-interface.ts
+++ src/app/interfaces/order-interface.ts
@@ -2,9 +2,10 @@ import { CartInterface } from './cart-interface';
 
 export interface OrderInterface {
   items: CartInterface[];
+  _id?: string;
   paymentType: string;
   address: any;
   data: Date;
   totalAmount: number;
-  status?: string
+  status?: string;
 }
