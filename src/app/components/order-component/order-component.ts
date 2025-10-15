541b0d1c163115da28736d20d09964f731c9dc3a
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 16:43:35 +0300

    add order service to order component

--- src/app/components/order-component/order-component.ts
+++ src/app/components/order-component/order-component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderInterface } from '../../interfaces/order-interface';
+import { OrderServices } from './../../Services/order-services';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-component',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './order-component.html',
-  styleUrl: './order-component.css'
+  styleUrl: './order-component.css',
 })
-export class OrderComponent {
-
+export class OrderComponent implements OnInit {
+  orders: OrderInterface[] = [];
+  constructor(private orderService: OrderServices) {}
+  ngOnInit(): void {
+    this.orderService.getOrders().subscribe((result) => {
+      this.orders = result;
+    });
+  }
 }
